8e5745ec40449e99b307636aad60455c187b1a46
Your Name <you@example.com>
2025-08-17 16:03:11 +0200

    ajout de la fonction receiveAttack

--- battleship.js
+++ battleship.js
@@ -31,11 +31,31 @@ class Gameboard {
         this.ships.push(ship);
         return ship
     }
+
+    receiveAttack(x, y) {
+        for (let ship of this.ships) {
+            if (ship.coordinates[0] === x && ship.coordinates[1] === y) {
+                ship.hit(); 
+                ship.checkIfSunk(); 
+                return "hit!";
+            }
+        }
+        return "miss!"; 
+    }
 }
+
 
 const board = new Gameboard();
 const newShip = board.placeShip(9, 9, 4, 0, "notSunk");
 console.log(newShip)
+
+board.receiveAttack(9, 9)
+board.receiveAttack(9, 9)
+board.receiveAttack(9, 9)
+board.receiveAttack(9, 9)
+
+console.log(newShip)
+
 
 module.exports = {
     Ship, Gameboard
