0f29cd529bb4e8ccc45b041699deb92eeba063b7
Your Name <you@example.com>
2025-09-04 11:33:27 +0200

    début d'une base DOM avec ajout Cell/button

--- battleship-dom.js
+++ battleship-dom.js
@@ -3,24 +3,36 @@
 
 // IIFE (Immediately Invoked Function Expression)
 // les paranthèses transforment la fonction en expression et non en déclaration
-(function() {
+(function () {
     function init() {
-        console.log("Init DOM"); 
+        console.log("Init DOM");
+        
+        // Initialize the game
+        const player1 = new Player(); 
+        const player2 = new Player();
+
+        
+
+
+
+        function ScreenController() {
 
         const boardDiv = document.querySelector(".board")
+        const playerTurnDiv = document.querySelector(".player-turn");
 
-        const board = new Gameboard(); 
-        board.grid.forEach((row, rowIndex) => {
+        const board1 = player1.ownBoard; 
+        board1.grid.forEach((row, rowIndex) => {
             row.forEach((cell, columnIndex) => {
-                const cellButton = document.createElement("button"); 
-                cellButton.classList.add("cell"); 
-                cellButton.dataset.row = rowIndex; 
+                const cellButton = document.createElement("button");
+                cellButton.classList.add("cell");
+                cellButton.dataset.row = rowIndex;
                 cellButton.dataset.column = columnIndex;
-                cellButton.text = cell.value; 
+                cellButton.text = cell.getValue();
                 boardDiv.appendChild(cellButton);
             })
         })
     }
+}
 
-    window.addEventListener("DOMContentLoaded", init);  
+    window.addEventListener("DOMContentLoaded", init);
 })();
