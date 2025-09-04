038236429203de0653ebd83b348433a1aad8c3bb
Your Name <you@example.com>
2025-09-04 11:11:34 +0200

    ajout début du module DOM

--- battleship-dom.js
+++ battleship-dom.js
@@ -6,6 +6,20 @@
 (function() {
     function init() {
         console.log("Init DOM"); 
+
+        const boardDiv = document.querySelector(".board")
+
+        const board = new Gameboard(); 
+        board.grid.forEach((row, rowIndex) => {
+            row.forEach((cell, columnIndex) => {
+                const cellButton = document.createElement("button"); 
+                cellButton.classList.add("cell"); 
+                cellButton.dataset.row = rowIndex; 
+                cellButton.dataset.column = columnIndex;
+                cellButton.text = cell.value; 
+                boardDiv.appendChild(cellButton);
+            })
+        })
     }
 
     window.addEventListener("DOMContentLoaded", init);  
