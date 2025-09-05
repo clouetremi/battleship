5878fd0a221a9542db74031cb1fb498b8fcb5e78
Your Name <you@example.com>
2025-09-05 11:18:54 +0200

    ajout de receiveAttack() dans le DOM et base CSS

--- battleship-dom.js
+++ battleship-dom.js
@@ -8,50 +8,63 @@
         console.log("Init DOM");
 
         // Initialize the game
-        function GameController(
-            playerOneName = "Player One",
-            playerTwoName = "Player Two"
-        ) {
-            const player1 = new Player();
-            const player2 = new Player();
+        const player1 = new Player();
+        const player2 = new Player();
 
-            const board1 = player1.ownBoard;
-            const board2 = player2.ownBoard;
+        const board1 = player1.ownBoard;
+        const board2 = player2.ownBoard;
 
-            let activePlayer = player1; 
+        let activePlayer = player1;
 
-            const switchPlayerTurn = () => {
-                activePlayer = activePlayer === player1 ? player2 : player1; 
-            }
+        const switchPlayerTurn = () => {
+            activePlayer = activePlayer === player1 ? player2 : player1;
+        }
 
-            const getActivePlayer = () => activePlayer; 
+        const getActivePlayer = () => activePlayer;
 
-            const printBoard = () => {
-                const boardWithCellValues = board1.map((row) => row.map((cell) => cell.getValue()));
-                console.log(boardWithCellValues)
-            }
-         }
 
-        function ScreenController() {
+        function renderBoard(board, container, isEnemy = false) {
 
-            const boardDiv = document.querySelector(".board")
-            const playerTurnDiv = document.querySelector(".player-turn");
+            container.textContent = ""; // reset l'affichage
 
-            // const board1 = player1.ownBoard; 
-            board1.grid.forEach((row, rowIndex) => {
-                row.forEach((cell, columnIndex) => {
+            board.grid.forEach((row, rowIndex) => {
+                const rowDiv = document.createElement("div");
+                rowDiv.classList.add("row");
+
+                row.forEach((cell, colIndex) => {
                     const cellButton = document.createElement("button");
                     cellButton.classList.add("cell");
                     cellButton.dataset.row = rowIndex;
-                    cellButton.dataset.column = columnIndex;
-                    cellButton.textContent = cell.value;
-                    boardDiv.appendChild(cellButton);
-                })
+                    cellButton.dataset.col = colIndex;
+
+                    if (isEnemy) {
+                        if (cell === "hit") cellButton.textContent = "hit";
+                        else if (cell === "missed") cellButton.textContent = "missed"; 
+                        else cellButton.textContent = ""; 
+                    } else {
+                        cellButton.textContent = cell; 
+                    }
+
+                    if (isEnemy) {
+                        cellButton.addEventListener("click", () => {
+                            board.receiveAttack(rowIndex, colIndex); 
+                            renderBoard(board, container, true);
+                        });
+                    }
+
+                    rowDiv.appendChild(cellButton); 
+                });
+
+                container.appendChild(rowDiv);
             })
         }
+        const board1Div = document.querySelector(".board1"); 
+        const board2Div = document.querySelector(".board2");
+    
+        renderBoard(board1, board1Div, false);
+        renderBoard(board2, board2Div, true);
+    };
 
-        ScreenController();
-    }
 
 
     window.addEventListener("DOMContentLoaded", init);
